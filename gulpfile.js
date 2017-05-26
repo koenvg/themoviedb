a83928da9d1bfe40abc47574418ad24c22ad3651
koenvg <koen.van.geert@gmail.com>
2017-05-26 16:54:26 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp");
+var plumber = require('gulp-plumber');
+var ts = require('gulp-typescript');
+var tslint = require('gulp-tslint');
+var stylish = require('gulp-tslint-stylish');
+var tsProject = ts.createProject('tsconfig.json');
+
+Error.stackTraceLimit = Infinity;
+
+gulp.task("build", ['lint'], function () {
+  return gulp.src(['src/ts/index.ts'])
+    .pipe(plumber())
+    .pipe(tsProject())
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('lint', function () {
+  gulp.src('src/**/*.ts')
+    .pipe(tslint())
+    .pipe(tslint.report(stylish, {
+      emitError: false,
+      sort: true,
+      bell: true
+    }));
+});
