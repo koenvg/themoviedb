0f3e8ff50c6f5307a2e220c9b0fc80527033b3a5
koenvg <koen.van.geert@gmail.com>
2017-06-10 15:35:38 +0200

    Fixed version number

--- src/ts/test.ts
+++
@@ -1,8 +0,0 @@
-// tslint:disable
-import themoveDb from './index';
-
-const { search } = themoveDb('58b2d591951178a249c5bb3cd2bbdb89');
-
-search('Game of thrones').then((results) => {
-  console.log(results);
-});
